a2e8793d4b37d13c8d65b538eef46354d0222107
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2025-02-02 13:25:47 +0500

    remove unused files

--- src/components/carousel/page.jsx
+++ src/components/carousel/page.jsx
@@ -70,8 +70,10 @@ const Carousel = ({ data, loading, endpoint, title }) => {
                 >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
-                    <CircleRating rating={item.vote_average.toFixed(1)} />
-                    <Genres data={item.genre_ids.slice(0, 2)} />
+                    <CircleRating
+                      rating={(item.vote_average || 0).toFixed(1)}
+                    />
+                    <Genres data={(item.genre_ids || []).slice(0, 2)} />
                   </div>
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
